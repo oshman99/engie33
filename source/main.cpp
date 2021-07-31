310bd0605c1c83abdd3b6569df474bb86fffccb4
Oshman <kraffo61@gmail.com>
2021-07-31 17:23:44 +0300

    added simple FPS camera controls, cleaned comments a bit

--- source/main.cpp
+++ source/main.cpp
@@ -12,11 +12,32 @@
 #include <shaderClass.h>
 #include <texture2DLoader.h>
 
+//сетап камеры
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//то, куда смотрит камера, аналогично yaw = 90.0f;
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+//перемнные для настройки плавности движения
+float deltaTime = 0.0f;//время между текущим и прошлым кадром
+float lastFrame = 0.0f;//время прошлого кадра
+
 //process key presses 
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    const float cameraSpeed = 2.5f * deltaTime;//adjust
+    //move forward/backwards
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    //strafe
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 //window resize callback
@@ -25,18 +46,65 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+//init mouse position variables(center of the screen)
+float lastX = 400, lastY = 300, yaw = -90.0f, pitch = 0.0f;
+//flag to check if this is the first time we recive mouse input
+bool firstMouse = true;
+
+//mouse movement callback
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+    
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
+float fov = 45.0f;
+
+//scrolling callback(zoom)
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov -= (float)yoffset;
+    if(fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+}
+
 int main()
 {
-    //------init GLFW-------
+    //------init GLFW, set everything and create window and context-------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //for MacOS only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    //--------
 
-    //--------set everything and create window and context---------
     GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL!", NULL, NULL);
     if (window == NULL)
     {
@@ -46,7 +114,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
     
-    //!!!загружаем функции OpenGL через GLAD!!!
+    //загружаем функции OpenGL через GLAD
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to init GLAD!" << std::endl;
@@ -136,8 +204,8 @@ int main()
          1, 2, 3    // second triangle
     };
 
-    //тестим класс для файлов шейдеров
-    //шейдеры копируются cmake в билд папку если шо
+    //класс для создание shader program из vertex и fragment шейдера, путь относительно билд папки
+    //TODO: Сделать более удобный доступ к шейдеру, может создать строку с путем в папку, что бы указывать только имя
     Shader ourShader("shaders/vertex.glsl", "shaders/fragment.glsl");
     //------------------------
 
@@ -155,49 +223,47 @@ int main()
     imgload::loadPNG("textures/container.png");
     //вторая текстура
     unsigned int texture2;
-
     glGenTextures(1, &texture2);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
-
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
     imgload::loadPNG("textures/da_dude.png");
-    //анбиндим
+    //анбиндим текстуры
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, 0);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, 0);
     //--------------------------
 
-    
     //------Creating vertex attributes and storing them in VAO------
     /*Создаем Vertex Array Object(VAO) - он позолит легко и быстро получать доступ к atrributes и VBO.
     Обычно когда в программе есть несколько объектов для отрисовки, сначала создаются
     и настраиваются все VAO(и соотвественно все VBO и attribute pointers), и сохраняются
     для использования далее. Когда нужно нарисовать один из объектов, нужный VAO биндится,
-    отрисоывается объект и VAO снова анбиндится*/
+    отрисоывается объект и VAO анбиндится*/
     unsigned int VAO, VAO2;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     //создаем Vertex Buffer Object для вершин.
-    //Сначала как обычный буфер с таким-то ID
+    //Сначала как буфер с таким-то ID
     unsigned int VBO, VBO2;
     glGenBuffers(1, &VBO);
 
     /*Затем биндим буфер к VBO-типу буфера GL_ARRAY_BUFFER
     OpenGL позволяет биндить множество буферов, 
     но только если они будут разного типа*/
+    //обычное правило - буферы НЕ меняющихся данных, юниформы для меняющихся данных
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     /*!!!С этого момента любой вызов буфера типа GL_ARRAY_TARGET 
-    будет изменять текущий буфер, забиндиный на этот тип !!!*/
+    будет изменять буфер, в данный момент забиндиный на этот тип !!!*/
 
     //Копируем вершины треугольника в буфер
-    /*4-ый параметр - то как ГПУ будет управляется с этими данными, куда их запихнет.
-    Параметр зависит от частоты установки и частоты использования этих данных. Смотри docs.gl*/
+    /*4-ый параметр - то как ГПУ будет управляется с этими данными, куда их запихнет в памяти GPU.
+    Память различается по скрости и типу доступа, так что стоит быть аккуратным.
+    Параметр обычно зависит от частоты изменения и использования этих данных. Смотри docs.gl*/
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 
     //Vertex atrribute берут данные из VBO, который сейчас привязан к GL_ARRAY_BUFFER
@@ -205,12 +271,12 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,3 * sizeof(float), (void*)0);
     /*тк сейчас привязан VAO и был передан 0 в первом параметре(и указан в шейдере)
     glVertexAttribPointer, vertex atrribute 0 теперь ассоциируются с position vertex attribute этого VAO.
-    Может быть много VertexAttributePointer и походу? можно переключаться между ними*/
+    Может быть много VertexAttributePointer и можно переключаться между ними*/
     glEnableVertexAttribArray(0);
     //анбиндим VAO - теперь он будет нужен только в render loop, тогда и забиндим
     glBindVertexArray(0);
 
-    //второй VAO
+    //второй VAO, 
     glGenVertexArrays(1, &VAO2);
     glBindVertexArray(VAO2);
     
@@ -266,17 +332,20 @@ int main()
     glEnable(GL_DEPTH_TEST);  
     //регистрация callback function, вызывающаяся когда изменяют размер окна. Эти функции могут вызываться при очень разных обстоятельствах
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    //тоже, но движение мыши
+    glfwSetCursorPosCallback(window, mouse_callback);
+    //тоже, но scrolling
+    glfwSetScrollCallback(window, scroll_callback);
+    //переход в view space(camera)
+    //функция умножающая на матрицу, которая "сдвигает" окружение как будто камера смторит на объект(1 - позиция камеры, 2 - на что смотреть, 3 - вектор направленный вверх)
+    glm::mat4 view;
     //glm, создаем матрицы для перехода в 3д
     //переход в world space. Устанавливаем при рендере
     glm::mat4 model = glm::mat4(1.0f);
     
-    //переход в view space(camera)
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     //переход в clip space(projection, frustum box). В нашем случае perspective
     glm::mat4 projection;
-    projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+    projection = glm::perspective(glm::radians(fov), 800.0f/600.0f, 0.1f, 100.0f);
     //setting texture unforms
     ourShader.use();
     glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
@@ -295,10 +364,10 @@ int main()
     //------render loop-------
     while(!glfwWindowShouldClose(window))
     {
-        //input
         processInput(window);
 
         //render commands go here!
+
         //выбор цвета для очистки экрана (state-setting func)
         glClearColor(0.4f, 0.3f, 0.3f, 1.0f);
         //установка бита колор буфера, очистка экрана заданным цветом(state-using func) и депф буфра
@@ -311,11 +380,17 @@ int main()
         glBindTexture(GL_TEXTURE_2D, texture2);
         //биндим VAO
         glBindVertexArray(VAO2);
-        //обновляем юниформы-матрицы
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         float offset = 0.0;
         ourShader.setFloat("offset", offset);
+        //обновляем переменные с временем кадров
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        //оюновляем view matrix, камера двигается!
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        //обновляем projection matrix, fov furstum box-a меняется
+        projection = glm::perspective(glm::radians(fov), 800.0f/600.0f, 0.1f, 100.0f);
+
         //обновляем model matrix что бы кубы раскидались по world view
         for(unsigned int i = 0; i< 10; i++)
         {
@@ -331,17 +406,18 @@ int main()
             //теперь рендерим
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-
+        //обновляем юниформы-матрицы
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         //Прямоугольник с EBO
         //glBindVertexArray(VAO_rec);
         //DrawElements берет индексы из текущего забинденого EBO. Собственно поэтому для облегчения их надо привязывать к VAO(как и все остальное! Core обязывает использовать VAO)
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
         //анбиндим VAO
         glBindVertexArray(0);
         //свапает бэк и фронт баферы по сути
         glfwSwapBuffers(window);
-        //проверяет сработало ли какое то событие, обновляет состояние окна и вызывает соотв. callback funtions
+        //проверяет сработало ли какое то событие, вызывает callback funtions из стека вроде(?) и обновляет состояние окна
         glfwPollEvents();
     }
     //---------------
